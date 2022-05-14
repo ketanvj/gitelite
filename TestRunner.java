6b6ed5deaffe28ff0c71ac4078b47464313aa8e9
Ketan J <ketanvj@yahoo.com>
2022-05-14 17:00:18 +0530

    Modified comments

--- TestRunner.java
+++ TestRunner.java
@@ -35,7 +35,7 @@ import io.cucumber.testng.FeatureWrapper;
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 	WebDriver driver;
-   // private TestNGCucumberRunner testNGCucumberRunner;
+   // private TestNGCucumberRunner testNGCucumberRunner; This is added from GitHub
   
        
 	/*
